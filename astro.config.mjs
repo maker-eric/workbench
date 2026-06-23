ad5a1d4585cfb77c3b2e48fe19469ef5108d081d
Eric <ej250@sohu.com>
2026-06-23 10:03:10 +0800

    production environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maker-eric.github.com/workbench',
+  base: '/workbench/',
   vite: {
     plugins: [tailwindcss()]
   }
